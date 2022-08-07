dd6481beb21ce20d9e082eb0a08e9bcbc4f269a8
전일훈 <ktdvlphun@gmail.com>
2022-08-08 03:24:19 +0900

    change

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  title: `Gatsby Starter Flat Blog`,
+  title: `GGURYS BLOG`,
   author: {
-    name: `Your Name`,
-    summary: `Your Summary`,
+    name: `IlHun Jeon`,
+    summary: `28`,
   },
-  copyright: `bottlehs`,
-  description: `Your Description`,
-  siteUrl: `https://ggury-blog.netlify.app`,
+  copyright: `ggurys`,
+  description: `28`,
+  siteUrl: `https://ggurys.com`,
   social: {
-    github: `https://github.com/bottlehs`,
+    github: `https://github.com/ktdvlp-jih`,
     instagram: `https://instagram.com`,
     twitter: `https://twitter.com`,
     facebook: `https://facebook.com`,
   },
 
   icon: `content/assets/app-icon.png`,
-  keywords: [`Gatsby`,`Blog`,`Gatsby Starter Blog`,`Netlify`,`JAMStack`],
+  keywords: [`Java`,`Blog`,`IlHun`,`Backend`,`Study`],
   googleAnalyticsId: 'UA-192500931-1',
   naverSiteVerification: '5ea388b83eab4834c0e497523f17618e578c68b9',
   disqus: {
     use: true,
-    shortname: 'ggury-blog'
+    shortname: 'ggurys'
   },
   buymeacoffee: {
     use: true,
-    url: `https://www.buymeacoffee.com/bottlehs`,
+    url: `https://www.buymeacoffee.com/ggurys`,
   }
 }
